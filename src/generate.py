754c315a4158eb0aa65c1b7f1e23d56b47d343cf
hakuxna <hakuxna@gmail.com>
2024-11-27 00:19:55 +0900

    📝 | update generate.py

--- src/generate.py
+++ src/generate.py
@@ -5,26 +5,37 @@ import os
 
 # -------------------------------------------------- フォントを生成するぞー！（オー！） --------------------------------------------------
 
-font = fontforge.font()
+fontfamily = [ext, exr, exb]
 
-# aのグリフを適当にいじる
-glyph = font.createChar(ord("a"), "a")
-pen = glyph.glyphPen()
-pen.moveTo((100, 100))
-pen.lineTo((200, 100))
-pen.lineTo((200, 200))
-pen.lineTo((100, 200)) # ちっちゃい正方形
-pen.closePath()
+# for文で、フォントの種類の数だけフォントを生成する
+for fontfamily in fontfamily:
 
-# フォントを生成する先のフォルダをつくっておく
-if os.path.isdir("out") == False:
-    os.mkdir("out")
-if os.path.isdir("out/ttf") == False:
-    os.mkdir("out/ttf")
-if os.path.isdir("out/woff") == False:
-    os.mkdir("out/woff")
+    font = fontforge.font()
 
-# いざ生成！
-font.generate("out/ttf/example.ttf")
-font.generate("out/woff/example.woff")
-font.close()
+    font.familyname = "???"
+    font.fullname = "???"
+    font.fontname = "???"
+    font.weight = "???"
+    font.copyright = "©2024 hakuxna"
+
+    # aのグリフを適当にいじる
+    glyph = font.createChar(ord("a"), "a")
+    pen = glyph.glyphPen()
+    pen.moveTo((100, 100))
+    pen.lineTo((200, 100))
+    pen.lineTo((200, 200))
+    pen.lineTo((100, 200)) # ちっちゃい正方形
+    pen.closePath()
+
+    # フォントを生成する先のフォルダをつくっておく
+    if os.path.isdir("out") == False:
+        os.mkdir("out")
+    if os.path.isdir("out/ttf") == False:
+        os.mkdir("out/ttf")
+    if os.path.isdir("out/woff") == False:
+        os.mkdir("out/woff")
+
+    # いざ生成！
+    font.generate("out/ttf/???.ttf")
+    font.generate("out/woff/???.woff")
+    font.close()
