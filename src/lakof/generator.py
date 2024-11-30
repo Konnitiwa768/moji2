4e1c5aa438efe2d76cd07985ca8be7a06f020ae2
hakuxna <hakuxna@gmail.com>
2024-11-30 18:09:54 +0900

    📝 | update lakof/generator.py

--- src/lakof/generator.py
+++ src/lakof/generator.py
@@ -6,10 +6,38 @@ import fontforge # 相変わらず「インポート "fontforge" を解決でき
 
 def lakof_generator(font, weight):
 
-    glyph = font.createChar(ord("a"), "a")
+    # フォントの太さは、下の表に対応する値の1/4ってことにする
+    # 100 | Thin
+    # 200 | ExtraLight
+    # 300 | Light
+    # 400 | Regular
+    # 500 | Medium
+    # 600 | SemiBold
+    # 700 | Bold
+    # 800 | Extrabold
+    # 900 | Black
+    # 例えばRegularだったら400 * 1/4 = 100
+    if weight == "Thin":
+        fontweight = 25
+    elif weight == "Regular":
+        fontweight = 100
+    elif weight == "Bold":
+        fontweight = 175
+
+    glyph = font.createChar(ord("l"), "l")
     pen = glyph.glyphPen()
-    pen.moveTo((100, 100))
-    pen.lineTo((200, 100))
-    pen.lineTo((200, 200))
-    pen.lineTo((100, 200))
+    
+    pen.moveTo((0, 0))
+    pen.lineTo((0, 500))
+    pen.lineTo((fontweight, 500))
+    pen.lineTo((fontweight, 0))
     pen.closePath()
+
+    pen.moveTo((0, 0))
+    pen.lineTo((0, fontweight))
+    pen.lineTo((400, fontweight))
+    pen.lineTo((400, 0))
+    pen.closePath()
+
+    glyph.left_side_bearing = 50
+    glyph.right_side_bearing = 50
