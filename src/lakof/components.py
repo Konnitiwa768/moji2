623537e6351f7a61c8a3c0749672c75153ed11f2
hakuxna <hakuxna@gmail.com>
2024-12-03 19:53:37 +0900

    📝 | update lakof/components.py, lakof/generator.py

--- src/lakof/components.py
+++ src/lakof/components.py
@@ -7,7 +7,7 @@ import fontforge # 「インポート "fontforge" を解決できませんでし
 # ココをいじるだけで見た目を改善できるようにする
 short_height = 500
 short_width = 400
-under_height = -300
+under_height = -500
 
 # 上のルールを受けて定義する部分
 long_height = short_height * 2
@@ -42,6 +42,14 @@ def shortRightVerticalBar(p, w):
     p.lineTo((sW - w, sH))
     p.lineTo((sW, sH))
     p.lineTo((sW, 0))
+    p.closePath()
+
+# 短縦棒（超右）
+def shortRightestVerticalBar(p, w):
+    p.moveTo(((sW - w) * 2, 0))
+    p.lineTo(((sW - w) * 2, sH))
+    p.lineTo((sW * 2 - w, sH))
+    p.lineTo((sW * 2 - w, 0))
     p.closePath()
 
 # 長縦棒
@@ -84,8 +92,24 @@ def shortForwardslashBar(p, w):
     p.lineTo((w, 0))
     p.closePath()
 
+# 長斜め棒（／）
+def longForwardslashBar(p, w):
+    p.moveTo((0, uH / 2))
+    p.lineTo((sW - w, (sH + lH) / 2))
+    p.lineTo((sW, (sH + lH) / 2))
+    p.lineTo((w, uH / 2))
+    p.closePath()
+
+# 超長斜め棒（／）
+def LongestForwardslashBar(p, w):
+    p.moveTo((0, uH))
+    p.lineTo((sW - w, lH))
+    p.lineTo((sW, lH))
+    p.lineTo((w, uH))
+    p.closePath()
+
 # 半円（左）
-def shortLeftCircle(p, w):
+def leftCircle(p, w):
     p.moveTo((hW, 0))
     p.curveTo((hW / 2, 0), (0, hH / 2), (0, hH))
     p.curveTo((0, (hH + sH) / 2), (hW / 2, sH), (hW, sH))
@@ -95,7 +119,7 @@ def shortLeftCircle(p, w):
     p.closePath()
 
 # 半円（右）
-def shortRightCircle(p, w):
+def rightCircle(p, w):
     p.moveTo((hW, 0))
     p.curveTo(((hW + sW) / 2, 0), (sW, hH / 2), (sW, hH))
     p.curveTo((sW, (hH + sH) / 2), ((sW + hW) / 2, sH), (hW, sH))
@@ -104,7 +128,7 @@ def shortRightCircle(p, w):
     p.curveTo((sW - w, (hH + w) / 2), ((sW - w + hW) / 2, w), (hW, w))
     p.closePath()
 
-# sとかの2画目とかにある「7」みたいなやつ
+# 短下カーブ
 def shortDownwardCurve(p, w):
     p.moveTo((w, sH * 0.9))
     p.curveTo(((hW + sW) / 2, sH * 1.2), (sW, sH), (sW, sH * 0.9))
@@ -117,5 +141,107 @@ def shortDownwardCurve(p, w):
     p.lineTo((sW, 0))
     p.closePath()
 
-# （……a、なんかスリムじゃない…………？）
+# 長下カーブ
+def longDownwardCurve(p, w):
+    p.moveTo((w, sH * 0.9))
+    p.curveTo(((hW + sW) / 2, sH * 1.2), (sW, sH), (sW, sH * 0.9))
+    p.lineTo((sW - w, sH * 0.9 - w))
+    p.curveTo((sW - w, sH * 0.9), (hW, sH), (w, sH * 0.9 - w))
+    p.closePath()
+    p.moveTo((sW, uH))
+    p.curveTo((sW - w, uH), (sW - w, 0), (sW - w, sH * 0.9 - w))
+    p.lineTo((sW, sH * 0.9))
+    p.curveTo((sW, 0), (sW, uH), (sW + w, uH))
+    p.closePath()
+
+# 短下カーブ（右）
+def shortRightDownwardCurve(p, w):
+    p.moveTo((sW, sH * 0.9))
+    p.curveTo(((hW + sW) / 2 - w + sW, sH * 1.2), (sW * 2 - w, sH), (sW * 2 - w, sH * 0.9))
+    p.lineTo(((sW - w) * 2, sH * 0.9 - w))
+    p.curveTo(((sW - w) * 2, sH * 0.9), (hW - w + sW, sH), (sW, sH * 0.9 - w))
+    p.closePath()
+    p.moveTo(((sW - w) * 2, 0))
+    p.lineTo(((sW - w) * 2, sH * 0.9 - w))
+    p.lineTo((sW * 2 - w, sH * 0.9))
+    p.lineTo((sW * 2 - w, 0))
+    p.closePath()
+
+# 長下カーブ（右）
+def longRightDownwardCurve(p, w):
+    p.moveTo((sW, sH * 0.9))
+    p.curveTo(((hW + sW) / 2 - w + sW, sH * 1.2), (sW * 2 - w, sH), (sW * 2 - w, sH * 0.9))
+    p.lineTo(((sW - w) * 2, sH * 0.9 - w))
+    p.curveTo(((sW - w) * 2, sH * 0.9), (hW - w + sW, sH), (sW, sH * 0.9 - w))
+    p.closePath()
+    p.moveTo((sW * 2 - w, uH))
+    p.curveTo(((sW - w) * 2, uH), ((sW - w) * 2, 0), ((sW - w) * 2, sH * 0.9 - w))
+    p.lineTo((sW * 2 - w, sH * 0.9))
+    p.curveTo((sW * 2 - w, 0), (sW * 2 - w, uH), (sW * 2, uH))
+    p.closePath()
+
+# 短上カーブ
+def shortUpwardCurve(p, w):
+    p.moveTo((sW - w, sH * 0.1))
+    p.curveTo((hW / 2, sH * -0.2), (0, 0), (0, sH * 0.1))
+    p.lineTo((w, sH * 0.1 + w))
+    p.curveTo((w, sH * 0.1), (hW, 0), (sW - w, sH * 0.1 + w))
+    p.closePath()
+    p.moveTo((w, sH))
+    p.lineTo((w, sH * 0.1 + w))
+    p.lineTo((0, sH * 0.1))
+    p.lineTo((0, sH))
+    p.closePath()
+
+# 長上カーブ
+def longUpwardCurve(p, w):
+    p.moveTo((sW - w, sH * 0.1))
+    p.curveTo((hW / 2, sH * -0.2), (0, 0), (0, sH * 0.1))
+    p.lineTo((w, sH * 0.1 + w))
+    p.curveTo((w, sH * 0.1), (hW, 0), (sW - w, sH * 0.1 + w))
+    p.closePath()
+    p.moveTo((0, lH))
+    p.curveTo((w, lH), (w, sH), (w, sH * 0.1 + w))
+    p.lineTo((0, sH * 0.1))
+    p.curveTo((0, sH), (0, lH), (-w, lH))
+    p.closePath()
+
+# 短上カーブ（右）
+def shortRightUpwardCurve(p, w):
+    p.moveTo(((sW - w) * 2, sH * 0.1))
+    p.curveTo((hW / 2 - w + sW, sH * -0.2), (sW - w, 0), (sW - w, sH * 0.1))
+    p.lineTo((sW, sH * 0.1 + w))
+    p.curveTo((sW, sH * 0.1), (hW - w + sW, 0), ((sW - w) * 2, sH * 0.1 + w))
+    p.closePath()
+    p.moveTo((sW, sH))
+    p.lineTo((sW, sH * 0.1 + w))
+    p.lineTo((sW - w, sH * 0.1))
+    p.lineTo((sW - w, sH))
+    p.closePath()
+
+# p用のしっぽ
+def shortPtail(p, w):
+    p.moveTo((0, 0))
+    p.curveTo((w, 0), (sW - w, hH), (sW - w, sH))
+    p.lineTo(sW, sH)
+    p.curveTo((sW, hH), (w * 2, 0), (w, 0))
+    p.closePath()
+
+# b用のしっぽ
+def shortBtail(p, w):
+    p.moveTo((0, uH))
+    p.curveTo((w, uH), (sW - w, 0), (sW - w, sH))
+    p.lineTo(sW, sH)
+    p.curveTo((sW, 0), (w * 2, uH), (w, uH))
+    p.closePath()
+
+# アポストロフィ
+def apostrophe(p, w):
+    p.moveTo(0, lH)
+    p.lineTo(0, (sH + lH) / 2)
+    p.lineTo(w, lH * 0.8)
+    p.lineTo(w, lH)
+
+# （……a、左右でぶった切られてるしなんかスリムじゃない…………？）
+# （……u、円と斜線が合わさって大変なことになってる？）
 # （……Widthの W とweightの w が入り混じって大変なことになってるな…………）
--- src/lakof/generator.py
+++ src/lakof/generator.py
@@ -17,10 +17,73 @@ def lakof_generator(font, weight):
     elif weight == "Bold":
         fontweight = 150
 
+    glyph = font.createChar(ord("p"), "p")
+    pen = glyph.glyphPen()
+    shortPtail(pen, fontweight)
+    shortRightUpwardCurve(pen, fontweight)
+    shortRightestVerticalBar(pen, fontweight)
+
+    glyph = font.createChar(ord("b"), "b")
+    pen = glyph.glyphPen()
+    shortBtail(pen, fontweight)
+    shortRightUpwardCurve(pen, fontweight)
+    shortRightestVerticalBar(pen, fontweight)
+
+    glyph = font.createChar(ord("k"), "k")
+    pen = glyph.glyphPen()
+    shortVerticalBar(pen, fontweight)
+    shortDownwardCurve(pen, fontweight)
+    shortRightDownwardCurve(pen, fontweight)
+
+    glyph = font.createChar(ord("g"), "g")
+    pen = glyph.glyphPen()
+    shortVerticalBar(pen, fontweight)
+    shortDownwardCurve(pen, fontweight)
+    longRightDownwardCurve(pen, fontweight)
+
+    glyph = font.createChar(ord("t"), "t")
+    pen = glyph.glyphPen()
+    shortUpwardCurve(pen, fontweight)
+    shortRightUpwardCurve(pen, fontweight)
+    shortRightestVerticalBar(pen, fontweight)
+
+    glyph = font.createChar(ord("d"), "d")
+    pen = glyph.glyphPen()
+    longUpwardCurve(pen, fontweight)
+    shortRightUpwardCurve(pen, fontweight)
+    shortRightestVerticalBar(pen, fontweight)
+
     glyph = font.createChar(ord("s"), "s")
     pen = glyph.glyphPen()
     shortVerticalBar(pen, fontweight)
     shortDownwardCurve(pen, fontweight)
+
+    glyph = font.createChar(ord("z"), "z")
+    pen = glyph.glyphPen()
+    shortVerticalBar(pen, fontweight)
+    longDownwardCurve(pen, fontweight)
+
+    glyph = font.createChar(ord("f"), "f")
+    pen = glyph.glyphPen()
+    shortUpwardCurve(pen, fontweight)
+    shortRightVerticalBar(pen, fontweight)
+
+    glyph = font.createChar(ord("v"), "v")
+    pen = glyph.glyphPen()
+    longUpwardCurve(pen, fontweight)
+    shortRightVerticalBar(pen, fontweight)
+
+    glyph = font.createChar(ord("c"), "c")
+    pen = glyph.glyphPen()
+    shortUpwardCurve(pen, fontweight)
+    shortRightVerticalBar(pen, fontweight)
+    shortRightDownwardCurve(pen, fontweight)
+
+    glyph = font.createChar(ord("q"), "q")
+    pen = glyph.glyphPen()
+    longUpwardCurve(pen, fontweight)
+    shortRightVerticalBar(pen, fontweight)
+    longRightDownwardCurve(pen, fontweight)
 
     glyph = font.createChar(ord("n"), "n")
     pen = glyph.glyphPen()
@@ -46,13 +109,32 @@ def lakof_generator(font, weight):
 
     glyph = font.createChar(ord("a"), "a")
     pen = glyph.glyphPen()
-    shortLeftCircle(pen, fontweight)
-    shortRightCircle(pen, fontweight)
+    leftCircle(pen, fontweight)
+    rightCircle(pen, fontweight)
+
+    glyph = font.createChar(ord("u"), "u")
+    pen = glyph.glyphPen()
+    leftCircle(pen, fontweight)
+    rightCircle(pen, fontweight)
+    longForwardslashBar(pen, fontweight)
 
     glyph = font.createChar(ord("0"), "0")
     pen = glyph.glyphPen()
     shortBackslashBar(pen, fontweight)
     shortForwardslashBar(pen, fontweight)
+
+    glyph = font.createChar(ord("."), ".")
+    pen = glyph.glyphPen()
+    LongestForwardslashBar(pen, fontweight)
+
+    glyph = font.createChar(ord("!"), "!")
+    pen = glyph.glyphPen()
+    LongestForwardslashBar(pen, fontweight)
+    shortBackslashBar(pen, fontweight)
+
+    glyph = font.createChar(ord("'"), "'")
+    pen = glyph.glyphPen()
+    apostrophe(pen, fontweight)
 
     # 縦の余白
     font.ascent = 1050
